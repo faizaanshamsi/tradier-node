31e53b1cb271ccebe3ee9191cb2beb26fc2cbb07
faizaanshamsi <faizaan2000@gmail.com>
2014-10-25 00:59:03 -0400

    working example

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var token = process.env.TOKEN
 
 var options = {
   hostname: 'sandbox.tradier.com',
-  path: '/v1/markets/options/chains?symbol=msft&expiration=2013-11-14',
+  path: '/v1/markets/options/chains?symbol=msft&expiration=2014-11-14',
   method: 'GET',
   headers: {'Accept': 'application/json',
             'Authorization': 'Bearer ' + token
@@ -17,7 +17,7 @@ var x = https.request(options, function(res){
     console.log("Connected");
     res.on('data',function(data){
         // data comes back as node buffer, can be converted to string or JSON
-        console.log(JSON.stringify(data));
+        // console.log(JSON.stringify(data));
         console.log(data.toString('utf-8'));
     });
 });

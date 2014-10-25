008e6d4108e3be70f71d68d1c04d4e47834d873b
faizaanshamsi <faizaan2000@gmail.com>
2014-10-25 00:46:06 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// var http = require("http");
+var https = require('https');
+
+var token = process.env.TOKEN
+
+var options = {
+  hostname: 'sandbox.tradier.com',
+  path: '/v1/markets/options/quotes?symbols=aapl',
+  method: 'GET',
+  headers: {'Accept': 'application/json',
+            'Authorization': 'Bearer ' + token
+  }
+};
+
+console.log("Start");
+var x = https.request(options, function(res){
+    console.log("Connected");
+    res.on('data',function(data){
+        // data comes back as node buffer, can be converted to string or JSON
+        console.log(JSON.stringify(data));
+        console.log(data.toString('utf-8'));
+    });
+});
+
+x.on('error',function(err){
+    console.log(err);
+});
+
+x.end();

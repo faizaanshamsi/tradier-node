3c67af8e5c7c4724da8d6dd3553e2bf5a903dbec
faizaanshamsi <faizaan2000@gmail.com>
2014-10-25 00:51:59 -0400

    hitting tradier server, get back null field for options.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var token = process.env.TOKEN
 
 var options = {
   hostname: 'sandbox.tradier.com',
-  path: '/v1/markets/options/quotes?symbols=aapl',
+  path: '/v1/markets/options/chains?symbol=msft&expiration=2013-11-14',
   method: 'GET',
   headers: {'Accept': 'application/json',
             'Authorization': 'Bearer ' + token
